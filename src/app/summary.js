b2f9add146ce25b02e44a0d65c5d770467c05ba1
Vioedge <scriptai.invest@yahoo.com>
2024-07-15 10:20:10 -0700

    scriptai

--- src/app/summary.js
+++ src/app/summary.js
@@ -8,7 +8,7 @@ export default function Home() {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const res = await fetch('https://videosummarysoftware.azurewebsites.net/summarize', {
+        const res = await fetch('https://videosummarybackend.azurewebsites.net/summarize', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
